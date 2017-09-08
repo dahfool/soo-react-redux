192c2d53faa60e314f91fe58acac437daeb63d29
Gerald Isabelle <info@geraldisabelle.me>
2017-09-08 01:57:16 +0100

    code refactor

--- src/js/component/app.js
+++ src/js/component/app.js
@@ -4,25 +4,24 @@ import Dropdown from '../containers/dropdown'
 import Input from '../containers/input'
 
 
+const App = (data) => {
 
-@connect((store) => {
-    return {
-      countries: store.search.country.countryList,
-      categories: store.search.category.categoryList,
-    }
-})
-export default class App extends Component {
-  render () {
     return (
-      <div>
-        <form>
-          <Input id='product' placeholder='Product type' aria='producthelp'>What do you sell ?</Input>
-          <Dropdown data={this.props.categories}/>
-          <Input id='country' placeholder='Country name' aria='countryhelp'>Where do you want to sell ?</Input>
-          <Dropdown  data={this.props.countries}/>
-          <button type='submit' className='btn btn-primary'>Find marketplaces</button>
-        </form>
-      </div>
+       <div>
+         <form>
+           <Input id='product' placeholder='Product type' aria='producthelp'>What do you sell ?</Input>
+           <Dropdown data={data.categories}/>
+           <Input id='country' placeholder='Country name' aria='countryhelp'>Where do you want to sell ?</Input>
+           <Dropdown  data={data.countries}/>
+           <button type='submit' className='btn btn-primary'>Find marketplaces</button>
+         </form>
+       </div>
     )
-  }
 }
+
+const mapStateToProps = (state) => ({
+    countries: state.search.country.countryList,
+    categories: state.search.category.categoryList
+})
+
+export default connect(mapStateToProps)(App);
